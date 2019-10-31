8e0bd687536bf33ad425e429335418eefdfb1e31
RohithNEU <kanuparti.k@huskey.neu.edu>
2019-10-30 21:29:50 -0400

    Changed to include a new drinks dictionary and some useful fields to store order

--- fulfillment/src/app.py
+++ fulfillment/src/app.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask import request
 from flask import make_response
 from flask import jsonify
+import firebase_admin
+from firebase_admin import credentials
+from google.cloud import firestore
 
 # initialize the flask app
 app = Flask(__name__)
@@ -11,10 +14,8 @@ app = Flask(__name__)
 def index():
     return 'Hello World!'
 
-# create a route for webhook
-# @app.route('/webhook')
-# def hello():
-#     return 'Hello World! from webhook '
+
+
 
 #function for responses
 def results():
@@ -22,9 +23,38 @@ def results():
     req = request.get_json(force=True)
     print(req)
     # fetch action from json
-    action = req.get('queryResult').get('action')
-    # return a fulfillment response
-    return {'fulfillmentText': 'This is a response from webhook.'}
+
+    #PARAMETERS FROM DIALOGFLOW
+    parameters =  req.get('queryResult').get('parameters')
+    drinkName = parameters.get('drink')[0]
+    drinkSize = parameters.get('size')[0]
+
+    #FIRESTORE INTERACTIONS
+    userId = u'myUserId'
+    db = firestore.Client()
+    doc_ref = db.collection(u'currentOrder').document(userId)
+    currentItemCount = doc_ref.get().to_dict().get(u'currentItemCount')
+    newCurrentItemCount = currentItemCount+1
+    drinksDict = doc_ref.get().to_dict().get(u'drinks')
+    if(drinksDict is None):
+        drinksDict = dict()
+    if drinkName in drinksDict:
+        drinksDict.get(drinkName)[newCurrentItemCount]=drinkSize
+    else:
+        drinksDict[drinkName] = {newCurrentItemCount : drinkSize}
+
+    doc_ref.update({
+        u'currentItemCount' : newCurrentItemCount,
+        u'drinks' : drinksDict
+        # u'drink': parameters.get('drink')[0],
+        # u'size': parameters.get('size')[0]
+    })
+    print( db.collection(u'users'))
+    return {'fulfillmentText': 'This is a response from webhook.Hi'}
+
+
+# def hit_firestore():
+
 
 # create a route for webhook
 @app.route('/webhook', methods=['GET', 'POST'])
