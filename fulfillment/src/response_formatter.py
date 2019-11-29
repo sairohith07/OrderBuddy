fb66559620a6847cac514325f7e2ca6c49f86213
Aanchal <samdariya.a@husky.neu.edu>
2019-11-29 12:55:16 -0500

    Merge branch 'master' of https://github.com/sairohith07/OrderBuddy
    # Conflicts:
    #	fulfillment/src/service.py

--- fulfillment/src/response_formatter.py
+++ fulfillment/src/response_formatter.py
@@ -16,9 +16,8 @@ class ResponseFormatter:
                                    + " " + ("" if customize_text.lower() == "no" else "With "+customize_text+" ")
         return response_string
 
-    def format_delete_last_item_response(self,last_item_num,item_desc):
-        print(last_item_num, item_desc)
-        response_string = "Are you sure you want to delete "+item_desc +" Please say "
+    def format_delete_last_item_response(self, last_item_num, item_desc):
+        response_string = "Are you sure you want to delete "+ item_desc +" Please say "
         response_string+= str(last_item_num)+ " to delete or  say No! to abort"
         print(response_string)
         return response_string
